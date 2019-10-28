465d1f4773392768cd57fe8804d58d77215b98a5
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-28 03:07:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist 
+  attr_accessor:name, :songs 
+  @@all = []
+  
+  def initialize (name)
+    @name = name 
+    @songs = song 
+  end 
+    
+    def add_song(song)
+      @songs << song
+    end 
+    
+    
+    
+     def save
+      @@all << self 
+    end
+end
